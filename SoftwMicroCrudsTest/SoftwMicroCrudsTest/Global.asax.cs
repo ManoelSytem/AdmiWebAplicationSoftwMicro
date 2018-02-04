b64a52f114b77828ef747dab8c94bd564991ed1e
manoelcontatosi@gmail.com <manoelcontatosi@gmail.com>
2018-02-04 19:37:07 -0200

    AdminSoftwMicro -  Desenvolvimento Aplicando Otimização.

--- SoftwMicroCrudsTest/SoftwMicroCrudsTest/Global.asax.cs
+++ SoftwMicroCrudsTest/SoftwMicroCrudsTest/Global.asax.cs
@@ -16,6 +16,8 @@ namespace SoftwMicroCrudsTest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
